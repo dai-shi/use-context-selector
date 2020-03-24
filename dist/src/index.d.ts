f05a7c9e4f733ef3e50c002ff2fff6d2c4a11612
daishi <daishi@axlight.com>
2020-03-24 09:49:18 +0900

    run compile; 2.0.0-alpha.2

--- dist/src/index.d.ts
+++ dist/src/index.d.ts
@@ -14,3 +14,17 @@ import { Context } from 'react';
 export declare function createContext<Value>(defaultValue: Value): Context<Value>;
 export declare function useContext<Value>(context: Context<Value>): Value;
 export declare function useContext<Value, Selected>(context: Context<Value>, selector: (value: Value) => Selected): Selected;
+declare type AnyCallback = (...args: any) => any;
+/**
+ * A utility function to wrap a callback function with higher priority
+ *
+ * Use this for a callback that will change a value,
+ * which will be fed into context provider.
+ *
+ * @example
+ * import { wrapCallbackWithPriority } from 'use-context-selector';
+ *
+ * const wrappedCallback = wrapCallbackWithPriority(callback);
+ */
+export declare function wrapCallbackWithPriority<Callback extends AnyCallback>(callback: Callback): Callback;
+export {};
