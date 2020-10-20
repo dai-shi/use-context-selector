bb618d68ef345b66520c01eb4e8f8392d6cc4c0e
mori <52605187+Morteza-Jenabzadeh@users.noreply.github.com>
2020-10-20 15:48:50 +0330

    Improve test (#28)
    * add a more detail test
    
    * revert button test
    
    * revert button test
    
    * find button by getByRole instead of getByText

--- __tests__/01_basic_spec.js
+++ __tests__/01_basic_spec.js
@@ -1,6 +1,8 @@
 import React, { useRef, useState, StrictMode } from 'react';
 
-import { render, fireEvent, cleanup } from '@testing-library/react';
+import {
+  render, fireEvent, cleanup, screen,
+} from '@testing-library/react';
 
 import {
   createContext,
@@ -35,18 +37,12 @@ describe('basic spec', () => {
     };
     const Counter2 = () => {
       const count2 = useContextSelector(context, (v) => v[0].count2);
-      const setState = useContextSelector(context, (v) => v[1]);
-      const increment = () => setState((s) => ({
-        ...s,
-        count2: s.count2 + 1,
-      }));
       const renderCount = useRef(0);
       renderCount.current += 1;
       return (
         <div>
           <span>{count2}</span>
-          <button type="button" onClick={increment}>+1</button>
-          <span>{renderCount.current}</span>
+          <span data-testid="counter2">{renderCount.current}</span>
         </div>
       );
     };
@@ -66,9 +62,10 @@ describe('basic spec', () => {
         </StateProvider>
       </StrictMode>
     );
-    const { getAllByText, container } = render(<App />);
+    const { container } = render(<App />);
     expect(container).toMatchSnapshot();
-    fireEvent.click(getAllByText('+1')[0]);
+    fireEvent.click(screen.getByRole('button'));
+    expect(screen.getByTestId('counter2').textContent).toEqual('1');
     expect(container).toMatchSnapshot();
   });
 });
